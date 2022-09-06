6361b583c6ffe0c73530d6f308c0ba1c93bb47f8
fanglong-it <Fang.longpc@gmail.com>
2022-09-06 10:19:11 +0700

    FangLong_Update_Source_BrandService

--- src/main/java/fiveman/hotelservice/service/BrandService.java
+++ src/main/java/fiveman/hotelservice/service/BrandService.java
@@ -1,7 +1,13 @@
 package fiveman.hotelservice.service;
 
 import fiveman.hotelservice.entities.Brand;
+import fiveman.hotelservice.entities.Device;
+
+import java.util.List;
 
 public interface BrandService {
 	Brand getBrandById(long id);
+	List<Brand> findAllProduct(int PAGE_INDEX, int PAGE_SIZE);
+	boolean addBrand(Device device);
+	boolean deleteBrand(long id);
 }
--- src/main/java/fiveman/hotelservice/service/Impl/BrandServiceImpl.java
+++ src/main/java/fiveman/hotelservice/service/Impl/BrandServiceImpl.java
@@ -1,8 +1,13 @@
 package fiveman.hotelservice.service.Impl;
 
+import java.util.List;
 import java.util.Optional;
 
+import fiveman.hotelservice.entities.Device;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -23,16 +28,32 @@ public class BrandServiceImpl implements BrandService{
 		if(brand != null) {
 			Brand dto = brand.orElseGet(null);
 			if(dto == null) {
-				
-//				productResponse res = new productResponse(dto.getProductID(), dto.getProductSKU(), dto.getProductName(), dto.getProductPrice(), dto.getProductWeight(), dto.getProductCartDesc(),
-//						dto.getProductShortDesc(), dto.getProductLongDesc(), dto.getProductThumb(), dto.getProductImage(), dto.getProductUpdateDate(), dto.getProductStock(), dto.getProductLive(),
-//						dto.getProductUnlimited(), listOption, listOptionGroups);
-//				return res;
 				throw new AppException(HttpStatus.NOT_FOUND.value(), "Not found id = "+id);   
 			}
 			return dto;
 		}
 		return null;
 	}
-	
+
+	@Override
+	public List<Brand> findAllProduct(int PAGE_INDEX, int PAGE_SIZE) {
+		Pageable pageable = PageRequest.of(PAGE_INDEX, PAGE_SIZE);
+		Page<Brand> brands = brandRepository.findAll(pageable);
+		if(brands.isEmpty()){
+			throw new AppException(HttpStatus.NOT_FOUND.value(), "Not found item!");
+		}else{
+			return brands.getContent();
+		}
+	}
+
+	@Override
+	public boolean addBrand(Device device) {
+		return false;
+	}
+
+	@Override
+	public boolean deleteBrand(long id) {
+		return false;
+	}
+
 }
